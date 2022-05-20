fdf237d7e0c493eb69cbe712b50158f221ccaf0c
Justin Lee <JustinDellAdam@live.com>
2022-05-20 12:41:24 +0800

    minor update

--- checkpoint_selector.py
+++ checkpoint_selector.py
@@ -17,12 +17,19 @@ BATCH_SIZE = config['TRAIN']['BATCH_SIZE']
 
 
 def select_proper_checkpoint():
+    min_loss = 99999
+    ideal_epoch = 0
     for epoch in arange(MIN_EPOCH, MAX_EPOCH + STEP_SIZE, STEP_SIZE):
         vocab = load_vocabulary()
         model = build_model(len(vocab.word2index), load_checkpoint=True, checkpoint_epoch=epoch, print_module=False)
         data_set = build_data_loader(batch_size=BATCH_SIZE)
         test_loss = model_evaluate(model, data_set)
         print('EPOCH %d Test PPL: %.4f' % (epoch, math.exp(test_loss)))
+        if min_loss > test_loss:
+            min_loss = test_loss
+            ideal_epoch = epoch
+
+    print("Ideal EPOCH: %d, Min PPL %.4f" % (ideal_epoch, math.exp(min_loss)))
 
 
 if __name__ == '__main__':
