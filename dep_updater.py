4919402bb49263b7d5f3b6ab676b0c6ab2c15e9a
Justin Lee <JustinDellAdam@live.com>
2022-07-16 02:04:17 +0800

    update deps

--- dep_updater.py
+++ dep_updater.py
@@ -3,4 +3,4 @@ from subprocess import call
 
 if __name__ == '__main__':
     packages = [dist.project_name for dist in pkg_resources.working_set]
-    call("pip install --upgrade " + ' '.join(packages), shell=True)
+    call("pip3 install --upgrade " + ' '.join(packages), shell=True)
